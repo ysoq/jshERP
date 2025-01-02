07eb32b305cd88b638307a7985904780cf5ae25e
赛 姚 <253711403@qq.com>
2025-01-02 13:35:06 +0800

    1

--- jshERP-web/src/views/system/InOutItemList.vue
+++ jshERP-web/src/views/system/InOutItemList.vue
@@ -1,87 +1,83 @@
 <template>
-  <a-row :gutter="24">
-    <a-col :md="24">
-      <a-card :style="cardStyle" :bordered="false">
+  <a-row :gutter='24'>
+    <a-col :md='24'>
+      <a-card :style='cardStyle' :bordered='false'>
         <!-- 查询区域 -->
-        <div class="table-page-search-wrapper">
+        <div class='table-page-search-wrapper'>
           <!-- 搜索区域 -->
-          <a-form layout="inline" @keyup.enter.native="searchQuery">
-            <a-row :gutter="24">
-              <a-col :md="6" :sm="24">
-                <a-form-item label="名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                  <a-input placeholder="请输入名称查询" v-model="queryParam.name"></a-input>
+          <a-form layout='inline' @keyup.enter.native='searchQuery'>
+            <a-row :gutter='24'>
+              <a-col :md='6' :sm='24'>
+                <a-form-item label='名称' :labelCol='labelCol' :wrapperCol='wrapperCol'>
+                  <a-input placeholder='请输入名称查询' v-model='queryParam.name'></a-input>
                 </a-form-item>
               </a-col>
-              <!-- <a-col :md="6" :sm="24">
-                <a-form-item label="类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                  <a-select v-model="queryParam.type" placeholder="请选择类型">
-                    <a-select-option value="">请选择</a-select-option>
-                    <a-select-option value="收入">收入</a-select-option>
-                    <a-select-option value="支出">支出</a-select-option>
-                  </a-select>
-                </a-form-item>
-              </a-col> -->
-              <a-col :md="6" :sm="24">
-                <a-form-item label="备注" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                  <a-input placeholder="请输入备注查询" v-model="queryParam.remark"></a-input>
+              <a-col :md='6' :sm='24'>
+                <a-form-item label='备注' :labelCol='labelCol' :wrapperCol='wrapperCol'>
+                  <a-input placeholder='请输入备注查询' v-model='queryParam.remark'></a-input>
                 </a-form-item>
               </a-col>
-              <span style="float: left; overflow: hidden" class="table-page-search-submitButtons">
-                <a-col :md="6" :sm="24">
-                  <a-button type="primary" @click="searchQuery">查询</a-button>
-                  <a-button style="margin-left: 8px" @click="searchReset">重置</a-button>
+              <span style='float: left; overflow: hidden' class='table-page-search-submitButtons'>
+                <a-col :md='6' :sm='24'>
+                  <a-button type='primary' @click='searchQuery'>查询</a-button>
+                  <a-button style='margin-left: 8px' @click='searchReset'>重置</a-button>
                 </a-col>
               </span>
             </a-row>
           </a-form>
         </div>
         <!-- 操作按钮区域 -->
-        <div class="table-operator" style="margin-top: 5px">
-          <a-button v-if="btnEnableList.indexOf(1) > -1" @click="handleAdd" type="primary" icon="plus">新增</a-button>
-          <a-button v-if="btnEnableList.indexOf(1) > -1" @click="batchDel" icon="delete">删除</a-button>
-          <a-button v-if="btnEnableList.indexOf(1) > -1" @click="batchSetStatus(true)" icon="check-square"
-            >启用</a-button
+        <div class='table-operator' style='margin-top: 5px'>
+          <a-button v-if='btnEnableList.indexOf(1) > -1' @click='handleAdd' type='primary' icon='plus'>新增</a-button>
+          <a-button v-if='btnEnableList.indexOf(1) > -1' @click='batchDel' icon='delete'>删除</a-button>
+          <a-button v-if='btnEnableList.indexOf(1) > -1' @click='batchSetStatus(true)' icon='check-square'
+          >启用
+          </a-button
           >
-          <a-button v-if="btnEnableList.indexOf(1) > -1" @click="batchSetStatus(false)" icon="close-square"
-            >禁用</a-button
+          <a-button v-if='btnEnableList.indexOf(1) > -1' @click='batchSetStatus(false)' icon='close-square'
+          >禁用
+          </a-button
           >
         </div>
         <!-- table区域-begin -->
         <div>
           <a-table
-            ref="table"
-            size="middle"
+            ref='table'
+            size='middle'
             bordered
-            rowKey="id"
-            :columns="columns"
-            :dataSource="dataSource"
-            :pagination="ipagination"
-            :scroll="scroll"
-            :loading="loading"
-            :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
-            @change="handleTableChange"
+            rowKey='id'
+            :columns='columns'
+            :dataSource='dataSource'
+            :pagination='ipagination'
+            :scroll='scroll'
+            :loading='loading'
+            :rowSelection='{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }'
+            @change='handleTableChange'
           >
-            <span slot="action" slot-scope="text, record">
-              <a @click="handleEdit(record)">编辑</a>
-              <a-divider v-if="btnEnableList.indexOf(1) > -1" type="vertical" />
+            <span slot='action' slot-scope='text, record'>
+              <a @click='handleEdit(record)'>编辑</a>
+              <a-divider v-if='btnEnableList.indexOf(1) > -1' type='vertical' />
               <a-popconfirm
-                v-if="btnEnableList.indexOf(1) > -1"
-                title="确定删除吗?"
-                @confirm="() => handleDelete(record.id)"
+                v-if='btnEnableList.indexOf(1) > -1'
+                title='确定删除吗?'
+                @confirm='() => handleDelete(record.id)'
               >
                 <a>删除</a>
               </a-popconfirm>
             </span>
+            <span slot='totalUnInAccount' slot-scope='enabled, record'>
+              {{ getPrice(record) }}
+            </span>
             <!-- 状态渲染模板 -->
-            <template slot="customRenderFlag" slot-scope="enabled">
-              <a-tag v-if="enabled" color="green">启用</a-tag>
-              <a-tag v-if="!enabled" color="orange">禁用</a-tag>
+            <template slot='customRenderFlag' slot-scope='enabled'>
+              <a-tag v-if='enabled' color='green'>启用</a-tag>
+              <a-tag v-if='!enabled' color='orange'>禁用</a-tag>
             </template>
           </a-table>
         </div>
         <!-- table区域-end -->
         <!-- 表单区域 -->
-        <inOutItem-modal ref="modalForm" @ok="modalFormOk"></inOutItem-modal>
+        <inOutItem-modal ref='modalForm' @ok='modalFormOk'></inOutItem-modal>
       </a-card>
     </a-col>
   </a-row>
@@ -91,21 +87,22 @@
 import InOutItemModal from './modules/InOutItemModal'
 import { JeecgListMixin } from '@/mixins/JeecgListMixin'
 import JDate from '@/components/jeecg/JDate'
+
 export default {
   name: 'InOutItemList',
   mixins: [JeecgListMixin],
   components: {
     InOutItemModal,
-    JDate,
+    JDate
   },
   data() {
     return {
       labelCol: {
-        span: 5,
+        span: 5
       },
       wrapperCol: {
         span: 18,
-        offset: 1,
+        offset: 1
       },
       // 查询条件
       queryParam: { name: '', type: '', remark: '' },
@@ -117,15 +114,20 @@ export default {
           dataIndex: 'action',
           width: 100,
           align: 'center',
-          scopedSlots: { customRender: 'action' },
+          scopedSlots: { customRender: 'action' }
         },
         { title: '名称', dataIndex: 'name', width: 200 },
         { title: '项目经理', dataIndex: 'username', width: 100 },
         { title: '联系方式', dataIndex: 'phonenum', width: 100 },
         { title: '合同金额', dataIndex: 'contractPrice', width: 100 },
-        // { title: '已回款金额', dataIndex: 'type', width: 100},
-        // { title: '未回款金额', dataIndex: 'type', width: 100},
-        // { title: '支出金额', dataIndex: 'type', width: 100},
+        { title: '已回款金额', dataIndex: 'totalInAccount', width: 100 },
+        {
+          title: '未回款金额',
+          dataIndex: 'totalUnInAccount',
+          width: 100,
+          scopedSlots: { customRender: 'totalUnInAccount' }
+        },
+        { title: '支出金额', dataIndex: 'totalOutAccount', width: 100 },
         { title: '备注', dataIndex: 'remark', width: 200 },
         { title: '排序', dataIndex: 'sort', width: 60 },
         {
@@ -133,28 +135,38 @@ export default {
           dataIndex: 'enabled',
           width: 60,
           align: 'center',
-          scopedSlots: { customRender: 'customRenderFlag' },
-        },
+          scopedSlots: { customRender: 'customRenderFlag' }
+        }
       ],
       url: {
         list: '/inOutItem/list',
         delete: '/inOutItem/delete',
         deleteBatch: '/inOutItem/deleteBatch',
-        batchSetStatusUrl: '/inOutItem/batchSetStatus',
-      },
+        batchSetStatusUrl: '/inOutItem/batchSetStatus'
+      }
     }
   },
   computed: {},
   methods: {
-    handleEdit: function (record) {
+    getPrice(record){
+      if(record.contractPrice >= 0) {
+        if(record.totalInAccount > record.contractPrice) {
+          return record.contractPrice
+        } else {
+          return (record.contractPrice - record.totalInAccount).toFixed(2)
+        }
+      }
+      return ''
+    },
+    handleEdit: function(record) {
       this.$refs.modalForm.edit(record)
       this.$refs.modalForm.title = '编辑'
       this.$refs.modalForm.disableSubmit = false
       if (this.btnEnableList.indexOf(1) === -1) {
         this.$refs.modalForm.isReadOnly = true
       }
-    },
-  },
+    }
+  }
 }
 </script>
 <style scoped>
