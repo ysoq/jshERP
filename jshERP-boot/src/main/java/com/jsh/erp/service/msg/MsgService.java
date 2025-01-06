73bc9fa48ad04f9e61c39295688907a9065f31ee
赛 姚 <253711403@qq.com>
2025-01-06 12:31:47 +0800

    新增盘点录入操作

--- jshERP-boot/src/main/java/com/jsh/erp/service/msg/MsgService.java
+++ jshERP-boot/src/main/java/com/jsh/erp/service/msg/MsgService.java
@@ -49,39 +49,39 @@ public class MsgService {
     @Resource
     private LogService logService;
 
-    public Msg getMsg(long id)throws Exception {
-        Msg result=null;
-        try{
-            result=msgMapper.selectByPrimaryKey(id);
-        }catch(Exception e){
+    public Msg getMsg(long id) throws Exception {
+        Msg result = null;
+        try {
+            result = msgMapper.selectByPrimaryKey(id);
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
         return result;
     }
 
-    public List<Msg> getMsg()throws Exception {
+    public List<Msg> getMsg() throws Exception {
         MsgExample example = new MsgExample();
         example.createCriteria().andDeleteFlagNotEqualTo(BusinessConstants.DELETE_FLAG_DELETED);
-        List<Msg> list=null;
-        try{
-            list=msgMapper.selectByExample(example);
-        }catch(Exception e){
+        List<Msg> list = null;
+        try {
+            list = msgMapper.selectByExample(example);
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
         return list;
     }
 
-    public List<MsgEx> select(String name, int offset, int rows)throws Exception {
-        List<MsgEx> list=null;
-        try{
+    public List<MsgEx> select(String name, int offset, int rows) throws Exception {
+        List<MsgEx> list = null;
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 list = msgMapperEx.selectByConditionMsg(userInfo.getId(), name, offset, rows);
                 if (null != list) {
                     for (MsgEx msgEx : list) {
@@ -91,25 +91,25 @@ public class MsgService {
                     }
                 }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
         return list;
     }
 
-    public Long countMsg(String name)throws Exception {
-        Long result=null;
-        try{
+    public Long countMsg(String name) throws Exception {
+        Long result = null;
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 result = msgMapperEx.countsByMsg(userInfo.getId(), name);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
@@ -117,21 +117,21 @@ public class MsgService {
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public int insertMsg(JSONObject obj, HttpServletRequest request)throws Exception {
+    public int insertMsg(JSONObject obj, HttpServletRequest request) throws Exception {
         Msg msg = JSONObject.parseObject(obj.toJSONString(), Msg.class);
-        int result=0;
-        try{
+        int result = 0;
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 msg.setCreateTime(new Date());
                 msg.setStatus("1");
-                result=msgMapper.insertSelective(msg);
+                result = msgMapper.insertSelective(msg);
                 logService.insertLog("消息",
                         new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_ADD).append(msg.getMsgTitle()).toString(), request);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
@@ -139,16 +139,16 @@ public class MsgService {
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public int updateMsg(JSONObject obj, HttpServletRequest request) throws Exception{
+    public int updateMsg(JSONObject obj, HttpServletRequest request) throws Exception {
         Msg msg = JSONObject.parseObject(obj.toJSONString(), Msg.class);
-        int result=0;
-        try{
-            result=msgMapper.updateByPrimaryKeySelective(msg);
+        int result = 0;
+        try {
+            result = msgMapper.updateByPrimaryKeySelective(msg);
             logService.insertLog("消息",
                     new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_EDIT).append(msg.getMsgTitle()).toString(), request);
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
@@ -156,15 +156,15 @@ public class MsgService {
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public int deleteMsg(Long id, HttpServletRequest request)throws Exception {
-        int result=0;
-        try{
-            result=msgMapper.deleteByPrimaryKey(id);
+    public int deleteMsg(Long id, HttpServletRequest request) throws Exception {
+        int result = 0;
+        try {
+            result = msgMapper.deleteByPrimaryKey(id);
             logService.insertLog("消息",
                     new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_DELETE).append(id).toString(), request);
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
@@ -172,68 +172,69 @@ public class MsgService {
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public int batchDeleteMsg(String ids, HttpServletRequest request) throws Exception{
+    public int batchDeleteMsg(String ids, HttpServletRequest request) throws Exception {
         List<Long> idList = StringUtil.strToLongList(ids);
         MsgExample example = new MsgExample();
         example.createCriteria().andIdIn(idList);
-        int result=0;
-        try{
-            result=msgMapper.deleteByExample(example);
+        int result = 0;
+        try {
+            result = msgMapper.deleteByExample(example);
             logService.insertLog("消息", "批量删除,id集:" + ids, request);
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
         return result;
     }
 
-    public int checkIsNameExist(Long id, String name)throws Exception {
+    public int checkIsNameExist(Long id, String name) throws Exception {
         MsgExample example = new MsgExample();
         example.createCriteria().andIdNotEqualTo(id).andMsgTitleEqualTo(name).andDeleteFlagNotEqualTo(BusinessConstants.DELETE_FLAG_DELETED);
-        List<Msg> list=null;
-        try{
-            list= msgMapper.selectByExample(example);
-        }catch(Exception e){
+        List<Msg> list = null;
+        try {
+            list = msgMapper.selectByExample(example);
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
-        return list==null?0:list.size();
+        return list == null ? 0 : list.size();
     }
 
     /**
      * create by: qiankunpingtai
-     *  逻辑删除角色信息
+     * 逻辑删除角色信息
      * create time: 2019/3/28 15:44
-     * @Param: ids
+     *
      * @return int
+     * @Param: ids
      */
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public int batchDeleteMsgByIds(String ids) throws Exception{
+    public int batchDeleteMsgByIds(String ids) throws Exception {
         logService.insertLog("序列号",
                 new StringBuffer(BusinessConstants.LOG_OPERATION_TYPE_DELETE).append(ids).toString(),
                 ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest());
-        String [] idArray=ids.split(",");
-        int result=0;
-        try{
-            result=msgMapperEx.batchDeleteMsgByIds(idArray);
-        }catch(Exception e){
+        String[] idArray = ids.split(",");
+        int result = 0;
+        try {
+            result = msgMapperEx.batchDeleteMsgByIds(idArray);
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
         return result;
     }
 
-    public List<MsgEx> getMsgByStatus(String status)throws Exception {
-        List<MsgEx> resList=new ArrayList<>();
-        try{
+    public List<MsgEx> getMsgByStatus(String status) throws Exception {
+        List<MsgEx> resList = new ArrayList<>();
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 MsgExample example = new MsgExample();
                 example.createCriteria().andStatusEqualTo(status).andUserIdEqualTo(userInfo.getId())
                         .andDeleteFlagNotEqualTo(BusinessConstants.DELETE_FLAG_DELETED);
@@ -254,9 +255,9 @@ public class MsgService {
                     }
                 }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
@@ -264,71 +265,70 @@ public class MsgService {
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public void batchUpdateStatus(String ids, String status) throws Exception{
+    public void batchUpdateStatus(String ids, String status) throws Exception {
         List<Long> idList = StringUtil.strToLongList(ids);
         Msg msg = new Msg();
         msg.setStatus(status);
         MsgExample example = new MsgExample();
         example.createCriteria().andIdIn(idList);
-        try{
+        try {
             msgMapper.updateByExampleSelective(msg, example);
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
     }
 
-    public Long getMsgCountByStatus(String status)throws Exception {
-        Long result=null;
-        try{
-            User userInfo=userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+    public Long getMsgCountByStatus(String status) throws Exception {
+        Long result = null;
+        try {
+            User userInfo = userService.getCurrentUser();
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 result = msgMapperEx.getMsgCountByStatus(status, userInfo.getId());
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
         return result;
     }
 
-    public List<Msg> getMsgCountByType(String type)throws Exception {
-        try{
+    public List<Msg> getMsgCountByType(String type) throws Exception {
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
-                MsgExample example = new MsgExample();
-                example.setOrderByClause("create_time desc");
-                example.createCriteria().andTypeEqualTo(type).andDeleteFlagNotEqualTo(BusinessConstants.DELETE_FLAG_DELETED);
-                List<Msg> list = msgMapper.selectByExample(example);
-                return list;
-            }
-        }catch(Exception e){
+//            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            MsgExample example = new MsgExample();
+            example.setOrderByClause("create_time desc");
+            example.createCriteria().andTypeEqualTo(type).andDeleteFlagNotEqualTo(BusinessConstants.DELETE_FLAG_DELETED);
+            List<Msg> list = msgMapper.selectByExample(example);
+            return list;
+//            }
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG,e);
+                    ExceptionConstants.DATA_READ_FAIL_CODE, ExceptionConstants.DATA_READ_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_READ_FAIL_CODE,
                     ExceptionConstants.DATA_READ_FAIL_MSG);
         }
-        return null;
     }
 
     @Transactional(value = "transactionManager", rollbackFor = Exception.class)
-    public void readAllMsg() throws Exception{
-        try{
+    public void readAllMsg() throws Exception {
+        try {
             User userInfo = userService.getCurrentUser();
-            if(!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
+            if (!BusinessConstants.DEFAULT_MANAGER.equals(userInfo.getLoginName())) {
                 Msg msg = new Msg();
                 msg.setStatus("2");
                 MsgExample example = new MsgExample();
                 example.createCriteria();
                 msgMapper.updateByExampleSelective(msg, example);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("异常码[{}],异常提示[{}],异常[{}]",
-                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG,e);
+                    ExceptionConstants.DATA_WRITE_FAIL_CODE, ExceptionConstants.DATA_WRITE_FAIL_MSG, e);
             throw new BusinessRunTimeException(ExceptionConstants.DATA_WRITE_FAIL_CODE,
                     ExceptionConstants.DATA_WRITE_FAIL_MSG);
         }
