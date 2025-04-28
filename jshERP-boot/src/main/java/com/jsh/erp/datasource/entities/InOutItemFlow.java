e7426797575085d971d5986ffbe966e2e635497f
ysok <253711403@qq.com>
2025-04-28 11:17:06 +0800

    refactor: 代码格式调整和简化 refactor: 使用lombok简化InOutItemFlow类

--- jshERP-boot/src/main/java/com/jsh/erp/datasource/entities/InOutItemFlow.java
+++ jshERP-boot/src/main/java/com/jsh/erp/datasource/entities/InOutItemFlow.java
@@ -1,94 +1,19 @@
 package com.jsh.erp.datasource.entities;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class InOutItemFlow {
     private Long projectId;
     private Long headerId;
-
     private String projectName;
     private String type;
     private String subType;
-
     private String number;
-
     private BigDecimal totalPrice;
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public Long getHeaderId() {
-        return headerId;
-    }
-
-    public void setHeaderId(Long headerId) {
-        this.headerId = headerId;
-    }
-
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getSubType() {
-        return subType;
-    }
-
-    public void setSubType(String subType) {
-        this.subType = subType;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
-
-    public Long getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(Long tenantId) {
-        this.tenantId = tenantId;
-    }
-
     private String createTime;
-
     private Long tenantId;
-
 }
--- jshERP-web/src/views/system/modules/InOutFlowModal.vue
+++ jshERP-web/src/views/system/modules/InOutFlowModal.vue
@@ -1,46 +1,47 @@
 <template>
-  <div ref='container'>
+  <div ref="container">
     <a-modal
-      :title='title'
-      :width='800'
-      :visible='visible'
-      :confirmLoading='confirmLoading'
-      :getContainer='() => $refs.container'
+      :title="title"
+      :width="800"
+      :visible="visible"
+      :confirmLoading="confirmLoading"
+      :getContainer="() => $refs.container"
       :maskStyle="{ top: '93px', left: '154px' }"
-      :wrapClassName='wrapClassNameInfo()'
-      :mask='isDesktop()'
-      :maskClosable='false'
-      @cancel='handleCancel'
-      cancelText='关闭'
-      okText='保存'
+      :wrapClassName="wrapClassNameInfo()"
+      :mask="isDesktop()"
+      :maskClosable="false"
+      @cancel="handleCancel"
+      cancelText="关闭"
+      okText="保存"
       :ok-button-props="{ style: { display: 'none' } }"
-      style='top: 10%; height: 80%'
+      style="top: 10%; height: 80%"
     >
-      <a-spin :spinning='confirmLoading'>
+      <a-spin :spinning="confirmLoading">
         <a-timeline>
-          <a-timeline-item :color='item.color' v-for='(item, index) in list' :key='index'>
-            <p style='margin-bottom: 5px;color: #555;font-size: 14px;font-weight: 600;'>时间：{{ format(item.createTime)}}  </p>
+          <a-timeline-item :color="item.color" v-for="(item, index) in list" :key="index">
+            <p style="margin-bottom: 5px;color: #555;font-size: 14px;font-weight: 600;">
+              时间：{{ format(item.createTime) }} </p>
             <div>
               <template v-if="item.type === '项目进度'">
-                <p style='margin-bottom: 3px'>{{ item.msgTitle }} <span
-                  v-if='item.projectStatus'>，项目进度：<a>{{ getProjectStatusText(item.projectStatus) }}</a></span></p>
-                <p v-if='item.msgContent'>进度：{{ item.msgContent }}</p>
-                <div style='display: flex;' v-if='item.recoverFile'>附件：
-                  <div><p v-for='(f,i) in item.recoverFile' :key='i'><a target='_blank'
-                                                                        :href='`/jshERP-boot/systemConfig/static/${f}`'>
+                <p style="margin-bottom: 3px">{{ item.msgTitle }} <span
+                  v-if="item.projectStatus">，项目进度：<a>{{ getProjectStatusText(item.projectStatus) }}</a></span></p>
+                <p v-if="item.msgContent">进度：{{ item.msgContent }}</p>
+                <div style="display: flex;" v-if="item.recoverFile">附件：
+                  <div><p v-for="(f,i) in item.recoverFile" :key="i"><a target="_blank"
+                                                                        :href="`/jshERP-boot/systemConfig/static/${f}`">
                     {{ f
                     }}</a></p></div>
                 </div>
               </template>
               <template v-else>
-                <p style='margin-bottom: 3px'>
-                  {{ item.subType ? item.subType.replace('销售', '材料') + "-" : '' }}{{ item.type }}
+                <p style="margin-bottom: 3px">
+                  {{ item.subType ? item.subType.replace('销售', '材料') + '-' : '' }}{{ item.type }}
                 </p>
-                <p style='margin-bottom: 3px'>
-                 单号：<a @click='showDetail(item)'>{{ item.number }}</a>
+                <p style="margin-bottom: 3px">
+                  单号：<a @click="showDetail(item)">{{ item.number }}</a>
                 </p>
                 <p>变动金额： <span
-                  style='color: orangered'>{{ ( item.type === '发票' ||  item.type === '收入' || item.subType === '销售退货' ? 1 : -1) * Math.abs(item.totalPrice)
+                  style="color: orangered">{{ (item.type === '发票' || item.type === '收入' || item.subType === '销售退货' ? 1 : -1) * Math.abs(item.totalPrice)
                   }}</span>元</p>
               </template>
             </div>
@@ -49,14 +50,14 @@
         </a-timeline>
       </a-spin>
 
-      <template slot='footer'>
+      <template slot="footer">
         <a-button @click='showDetail({type: "收支汇总", inOutItemId: `${record.id}`})'>
           收支汇总
         </a-button>
         <a-button @click='showDetail({type: "领料汇总", inOutItemId: `${record.id}`})'>
           领料汇总
         </a-button>
-        <a-button type='primary' @click='handleCancel'>
+        <a-button type="primary" @click="handleCancel">
           关闭
         </a-button>
       </template>
@@ -72,7 +73,7 @@ import { getProjectStatusText } from '@views/system/InOutItemCommon'
 export default {
   name: 'InOutFlowModal',
   mixins: [mixinDevice],
-  data() {
+  data () {
     return {
       title: '项目情况',
       visible: false,
@@ -81,14 +82,14 @@ export default {
       record: null
     }
   },
-  created() {
+  created () {
   },
   methods: {
     getProjectStatusText,
-    format(val) {
+    format (val) {
       return dayjs(val).format('YYYY-MM-DD HH:mm:ss')
     },
-    edit(record) {
+    edit (record) {
       this.visible = true
       this.confirmLoading = true
       this.record = record
@@ -109,14 +110,14 @@ export default {
         this.confirmLoading = false
       })
     },
-    close() {
+    close () {
       this.$emit('close')
       this.visible = false
     },
-    handleCancel() {
+    handleCancel () {
       this.close()
     },
-    showDetail(item) {
+    showDetail (item) {
       let link = ''
       window.localStorage.setItem('flowItem', JSON.stringify(item))
       if (item.type === '收支汇总') {
@@ -127,6 +128,8 @@ export default {
         link = this.$router.resolve(`/financial/item_in`)
       } else if (item.type === '支出') {
         link = this.$router.resolve(`/financial/item_out`)
+      } else if (item.type === '发票') {
+        link = this.$router.resolve(`/financial/invoice_record`)
       } else if (item.code.startsWith('QTCK')) {
         link = this.$router.resolve(`/bill/other_out`)
       } else if (item.code.startsWith('XSCK')) {
